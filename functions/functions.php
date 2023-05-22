16f4a9f2595f1e1b58c42a4b8d9e1778ed65694f
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-22 20:04:42 +0200

    Creazione file separato functions.php

---
+++ functions/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+// funzione per la generazione di caratteri casuali compresi numeri, lettere minuscole, maiuscole e caratteri speciali
+function getRandomString($n){
+    
+    // array di numeri, lettere minuscole, maiuscole e caratteri speciali
+    $characters = [
+        '0123456789', // numbers
+        'abcdefghijklmnopqrstuvwxyz', //lettersm
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ', //lettersM
+        '\|!"£$%&/()=?@[]{}+-_°#ç§*' //symbols
+    ];
+    
+    $randomString = '';
+
+    var_dump( $characters );
+    
+    // creazione ciclo for per generare la quantita di caratteri da creare
+    for ($i = 0; $i < $n; $i++) {
+
+        $indice = $i;
+
+        // quando l'indice supera la lunghezza dell'array $characters genero un numero random indice casuale che mi darà dei numeri casuali di volta in volta in base a dove l'indice verrà posizionato ad ogni giro del ciclo
+        if ( $indice > 3 ) {
+
+            $indice = rand( 0, sizeof($characters) );
+            $indexRand = rand( 0, strlen($characters[$indice]));            
+            $randomString .= $characters[$indice][$indexRand];
+
+        } else {
+
+            $indexRand = rand( 0, strlen($characters[$indice]) - 1 );
+            $randomString .= $characters[$indice][$indexRand];
+        }
+    }
+
+    return $randomString;
+}
--- index.php
+++ index.php
@@ -1,53 +1,11 @@
 <?php
 
-// funzione per la generazione di caratteri casuali compresi numeri, lettere minuscole, maiuscole e caratteri speciali
-function getRandomString($n){
-    
-    $characters = [
-        '0123456789', // numbers
-        'abcdefghijklmnopqrstuvwxyz', //lettersm
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZ', //lettersM
-        '\|!"£$%&/()=?@[]{}+-_°#ç§*' //symbols
-    ];
-    
-    $randomString = '';
-    var_dump( $characters );
-
-    var_dump( rand( 0, strlen($characters[rand( 0, sizeof($characters) )])) );
-    
-    // creazione ciclo for per generare la quantita di caratteri da creare
-    for ($i = 0; $i < $n; $i++) {
-
-        $indice = $i;
-
-        // quando l'indice supera la lunghezza dell'array $characters genero un numero random indice casuale che mi darà dei numeri casuali di volta in volta in base a dove l'indice verrà posizionato ad ogni giro del ciclo
-        if ( $indice > 3 ) {
-
-            $indice = rand( 0, sizeof($characters) );
-
-            $indexRand = rand( 0, strlen($characters[$indice]));
-            
-            $randomString .= $characters[$indice][$indexRand];
-
-        } else {
-
-            $indexRand = rand( 0, strlen($characters[$indice]) - 1 );
-
-            $randomString .= $characters[$indice][$indexRand];
-        }
-    }
-
-    return $randomString;
-
-}
+include './functions/functions.php';
 
 // se il form invia un input mi fa partire la funzione 
 if ( isset($_GET['passwordLenght']) ) {
-
     echo getRandomString($_GET['passwordLenght'] );
 }
-
-var_dump($_GET['passwordLenght'] );
 
 ?>
 
