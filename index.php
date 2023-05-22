0ced0bd6d6cb631b7ad2d078a6890f1907d54e7d
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-22 15:25:25 +0200

    Creazione form

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+var_dump($_GET['password'] );
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <title>Strong Password Generator</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center mt-4">
+            <div class="col-5">
+
+                <!-- form -->
+                <form action="index.php"  method="GET">
+                    <div class="mb-3">
+                        <label for="generaPass" class="form-label">Crea la tua password</label>
+                        <input type="text" class="form-control" id="generaPass" name="password">
+                    </div>
+                    
+                    <!-- <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
